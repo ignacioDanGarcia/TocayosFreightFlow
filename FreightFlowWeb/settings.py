8fac0e7a682eb63ef9d35f4419be1b856a768379
Ignacio Daniel García <igniciagar@gmail.com>
2023-12-08 17:08:58 -0300

    cambio staticfiles dirs

--- FreightFlowWeb/settings.py
+++ FreightFlowWeb/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "HomeTffApp" / "static",
 ]
 STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfilesbuild', 'static')
 # Default primary key field type
