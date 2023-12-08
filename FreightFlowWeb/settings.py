7359665c9fb5d00981bcbf51eb5544c5a201cb8a
Ignacio Daniel García <igniciagar@gmail.com>
2023-12-08 14:06:47 -0300

    prueba vercel2

--- FreightFlowWeb/settings.py
+++ FreightFlowWeb/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'FreightFlowWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '44G2GcCG2AFa324aGdD5E1aaA*BG2daF',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '51485',
     }
 }
 
@@ -116,10 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_URL = '/static/'
+STATICFILES_DIRS =  [os.path.join(BASE_DIR, 'HomeTffApp', 'static')]
+STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfilesbuild', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
